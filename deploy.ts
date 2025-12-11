6b915e34148ba2d476f953d0b2f90fed3cd4ca06
Budder <austin.joseph.wallin@gmail.com>
2025-12-11 17:12:13 -0600

    Delete deploy.ts

--- deploy.ts
+++
@@ -1,9 +0,0 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
-declare const Deno: any;
-
-Deno.serve((req: Request) => {
-  return serveDir(req, {
-    fsRoot: "dist",
-  });
-});
